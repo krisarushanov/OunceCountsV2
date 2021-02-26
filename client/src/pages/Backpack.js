437ce0b85b4213fb4c001c89da25e6cbd38d2a77
Kristena Arushanov <kris.arushanov@gmail.com>
2021-02-25 17:14:23 -0800

    added Filter Button

---
+++ client/src/pages/Backpack.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Todo from "../components/Backpack/Todo";
+import '../components/Backpack/index.css';
+import Form from'../components/Backpack/Form';
+import '../components/Backpack/FilterButton'
+import FilterButton from '../components/Backpack/FilterButton';
+import Container from '../components/Grid/Container';
+import Col from '../components/Grid/Col';
+
+
+function addTask(name) {
+  alert(name);
+}
+
+function Backpack(props) {
+    return (
+   <Container>
+    
+        <div className="todoapp stack-large">
+         <Form addTask={addTask} />
+          <div className="filters btn-group stack-exception">
+            <FilterButton />
+            <FilterButton />
+            <FilterButton />
+          </div>
+          <h2 id="list-heading">Items in you Backpack</h2>
+          <ul
+            role="list"
+            className="todo-list stack-large stack-exception"
+            aria-labelledby="list-heading"
+          >
+          <Todo name = "Example" completed={false} id="todo-0"/>
+          <Todo name= "Example" completed={false} id ="todo-1"/>
+          <Todo name= "Example" completed={false} id="todo-2"/>
+          
+        
+        </ul>
+
+      </div>
+      
+
+      </Container>
+    );
+  }
+
+  export default Backpack;
