96413805bf858ac9df87d4ce7e459d33bdb46703
jsvalle123 <68571871+jsvalle123@users.noreply.github.com>
2021-02-25 20:37:39 -0800

    get

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const alphapackRoutes = require("./alphapack");
-const userRoutes = require("./user");
+// const userRoutes = require("./user");
 const wolfpackBackpackRoutes = require("./backpack");
 
 
 //alpha wolfpack routes
 router.use("/alphapack", alphapackRoutes);
 // define user routes
-router.use("/user", userRoutes);
+// router.use("/user", userRoutes);
 // define wolfpack-backpack-routes routes
 router.use("/backpack", wolfpackBackpackRoutes);
 
