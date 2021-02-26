d1833d8c5ee508f6f64982a5d48f3b6668c8e324
Jaira Arcilla <arcilla.jaira@gmail.com>
2021-02-25 18:23:09 -0800

    added alphapack page

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 import Nav from './Nav.js';
 import About from './pages/About';
 import Backpack from './pages/Backpack';
-
-//import Alphapack from './pages/Alphapack';
+import Alphapack from './pages/Alphapack';
 //import Sandbox from './pages/Sandbox';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -18,6 +17,7 @@ function App() {
       {/* <Route exact path="/" component={Login} /> */}
         {/* <Route path ="/sandbox" component={Sandbox}/> */}
         <Route path="/about" component={About}/>
+        <Route path="/alphapack" component={Alphapack}/>
         <Route path="/backpack" component={Backpack}/>
         {/* <Route path="/alphapack" component={Alphapack}/> */}
       </Switch>
