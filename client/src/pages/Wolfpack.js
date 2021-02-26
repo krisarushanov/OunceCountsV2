0edb1fe61211747be261f6d4d4d7ea2ccbcfc93b
jsvalle123 <68571871+jsvalle123@users.noreply.github.com>
2021-02-25 16:34:44 -0800

    start page

--- client/src/pages/Wolfpack.js
+++ client/src/pages/Wolfpack.js
@@ -1,3 +1,15 @@
 import React from "react";
+import Nav from "../components/Nav";
 import ContainerChat from "../components/ContainerChat";
 import EmergencyCard from "../components/EmergencyCard";
+import Jumbotron from "../components/Jumbotron";
+
+const Wolfpack = () => {
+    return (
+        <Nav >
+            <ContainerChat>
+
+            </ContainerChat>
+        </Nav>
+    )
+}
