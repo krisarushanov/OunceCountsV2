1eafea24287104fff34a29dd2ef2fc0521cbb6b0
Kristena Arushanov <kris.arushanov@gmail.com>
2021-02-26 22:41:18 -0800

    page formatting

--- client/src/pages/Backpack.js
+++ client/src/pages/Backpack.js
@@ -8,16 +8,12 @@ import '../components/Backpack/FilterButton'
 import FilterButton from '../components/Backpack/FilterButton';
 import Container from '../components/Grid/Container';
 
-
-
 function addTask(name) {
   alert(name);
 }
 
 function Backpack(props) {
     return (
-
-  
  <Container>
         
         <div className="row" > 
@@ -32,25 +28,21 @@ function Backpack(props) {
         </div>
         </div>
         
-        
         <div className="row" > 
-        <div className="col-md-5" id="colTwo"> 
-        <div className="card mb-4"id="cardTwo" >
-        <div className="card-body"  > 
-        <h1 style={{fontSize:"50px",fontWeight:"800", letterSpacing: "1.13px"}}>Wolfpack</h1>
-        <h2 style={{fontSize:"25px"}} >Collaborate on your adventure Backpack
-        with your Wolfpack to stay safe and avoid overpacking.</h2>
+         <div className="col-md-5" id="colTwo"> 
+          <div className="card mb-4"id="cardTwo" >
+            <div className="card-body"  > 
+              <h1 style={{fontSize:"50px",fontWeight:"800", letterSpacing: "1.13px"}}>Wolfpack</h1>
+              <h2 style={{fontSize:"25px"}} >Collaborate on your adventure Backpack
+              with your Wolfpack to stay safe and avoid overpacking.</h2>
         </div>
-        </div>
-        </div>
-        </div>
-        </div>
+          </div>
+            </div>
+              </div>
+                </div>
         
         <div className="col"> 
         <div className="todoapp stack-large">
-
- 
-    
          <Form addTask={addTask} />
           <div className="filters btn-group stack-exception">
             <FilterButton />
@@ -65,20 +57,12 @@ function Backpack(props) {
           >
           <Todo name = "First-aid Kit" completed={false} id="todo-0"/>
           <Todo name= "GPS Navigation" completed={false} id ="todo-1"/>
-          <Todo name= "Fire Starter" completed={false} id="todo-2"/>
-          
-        
+          <Todo name= "Fire Starter" completed={false} id="todo-2"/>     
         </ul>
         </div>
-     
-      </div>
-      </div>
-    
-      
-      
-   
-   </Container>
-      
+          </div>
+            </div>
+          </Container>
     );
   }
 
