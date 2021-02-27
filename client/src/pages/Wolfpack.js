d0983b478046dfb50d011fb8653fb1e08ea2f9c2
jsvalle123 <68571871+jsvalle123@users.noreply.github.com>
2021-02-27 08:37:53 -0800

    fixe conflicts

--- client/src/pages/Wolfpack.js
+++ client/src/pages/Wolfpack.js
@@ -1,10 +1,6 @@
 import React from "react";
 import EmergencyCard from "../components/EmergencyCard";
 import Jumbotron from "../components/Jumbotron";
-<<<<<<< HEAD
-import {SocketContext, socket} from 'context/socket';
-import Child from 'components/Child';
-=======
 import ContainerChat from "../components/ContainerChat";
 import profile from '../images/bgoc.jpeg';
 import UsersContainer from '../components/UsersContainer';
@@ -12,7 +8,6 @@ import BackpackLink from '../components/BackpackLink';
 import './style.css';
 
 
->>>>>>> 1a88014c06d51bb68db03d49881b7731c2ecdcb6
 
 const Wolfpack = (props) => {
     
@@ -23,21 +18,12 @@ const Wolfpack = (props) => {
                 <h1>Wolfpack Name: Adventure Time</h1>
                 <h2>Alpha: Harry White</h2>
             </Jumbotron>
-<<<<<<< HEAD
-            <SocketContext.Provider value={socket}>
-      <Child />
-      <Child />
-      ...
-    </ SocketContext.Provider>
-
-=======
             <BackpackLink />
                 <EmergencyCard emergencyContactObj={{ fName: "Ben", lName: "Smith", pNum: "2145552456" }} />
                 <UsersContainer memberObj={{ name: "John Snow"}} />
                 <UsersContainer memberObj={{ name: "John Snow"}} />
                 <UsersContainer memberObj={{ name: "John Snow"}} />
                 <UsersContainer memberObj={{ name: "John Snow"}} />
->>>>>>> 1a88014c06d51bb68db03d49881b7731c2ecdcb6
 
        <ContainerChat />
     </div>
